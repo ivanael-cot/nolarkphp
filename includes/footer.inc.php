02511b9000723e1d3e4d51a2c1b1e359debed57e
Ivanaël Cot <ivanael.cot@gmail.com>
2026-03-02 12:34:44 +0100

    Inititalisation nolarkphp ("page-6")

---
+++ includes/footer.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+echo '<footer>
+            <p>&copy;2018 Nolark - Créé par <a href="mailto:jose.gil@ac-nice.fr">José GIL & Ivanaël</a><p>
+     </footer>';
--- includes/lienspages.inc.php
+++ includes/lienspages.inc.php
@@ -1,11 +1,12 @@
 <?php
- $pages = ['route.html', 'cross.html', 'piste.html', 'enfants.html',
- 'nous-contacter.html'];
- $noms = ['Route', 'Cross', 'Piste', 'Enfants', 'Nous contacter'];
- $nbLiens = count($pages);
- for ($i=0; $i < $nbLiens; $i++) {
- echo '<li><a href="pages/', $pages[$i], '">', $noms[$i], '</a></li>';
- }
+
+$pages = ['route.html', 'cross.html', 'piste.html', 'enfants.html',
+    'nous-contacter.html'];
+$noms = ['Route', 'Cross', 'Piste', 'Enfants', 'Nous contacter'];
+$nbLiens = count($pages);
+for ($i = 0; $i < $nbLiens; $i++) {
+    echo '<li><a href="pages/', $pages[$i], '">', $noms[$i], '</a></li>';
+}
 /* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
