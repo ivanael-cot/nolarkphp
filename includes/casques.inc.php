bc60e9faf799f99c568dd0b31235b7b6038eae7c
IvanaelCot <ivanael.cot@gmail.com>
2026-03-08 22:36:33 +0100

    fixbug

--- includes/casques.inc.php
+++ includes/casques.inc.php
@@ -1,5 +1,14 @@
+$php
 <?php
-$cnx = new PDO('mysql:host=127.0.0.1;dbname=nolark', 'nolarkuser', 'nolarkpwd');
+// la connexion PDO doit indiquer le port utilisé par MySQL sous MAMP (souvent 8889)
+// et gérer proprement les erreurs pour éviter le fatal error "Connection refused".
+$dsn = 'mysql:host=127.0.0.1;port=8889;dbname=nolark;charset=utf8';
+try {
+	$cnx = new PDO($dsn, 'nolarkuser', 'nolarkpwd');
+	$cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	die('Erreur de connexion à la base de données : ' . $e->getMessage());
+}
 $req = 'SELECT casque.id, nom, modele, libelle, prix, classement, image, stock';
 $req .= ' FROM casque INNER JOIN type ON casque.type=type.id';
 $req .= ' INNER JOIN marque ON casque.marque=marque.id';
