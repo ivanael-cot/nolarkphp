2a0a08044e50e664b9551f8908376cddb1d7ce1c
IvanaelCot <ivanael.cot@gmail.com>
2026-03-08 22:53:24 +0100

    fix db

--- includes/casques.inc.php
+++ includes/casques.inc.php
@@ -11,9 +11,7 @@ $res = $cnx->query($req);
 echo '<section id="casques">';
 while ($ligne = $res->fetch(PDO::FETCH_OBJ)) {
 echo '<article>';
-echo '<img src="../images/casques/', $ligne->libelle, '/', $ligne->image,
-'" alt="', $ligne->modele,
-'">';
+echo '<img src="../images/casques/', $ligne->libelle, '/', $ligne->image,'" alt="', $ligne->modele,'">';
 echo '</article>';
 }
 echo '</section>';
