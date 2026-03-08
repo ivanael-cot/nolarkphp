928acde38003a76da7c63db8801dc2aa3ac7e14f
IvanaelCot <ivanael.cot@gmail.com>
2026-03-08 22:29:54 +0100

    fin du TP nolarkphp

---
+++ includes/casques.inc.php
@@ -0,0 +1,18 @@
+<?php
+$cnx = new PDO('mysql:host=127.0.0.1;dbname=nolark', 'nolarkuser', 'nolarkpwd');
+$req = 'SELECT casque.id, nom, modele, libelle, prix, classement, image, stock';
+$req .= ' FROM casque INNER JOIN type ON casque.type=type.id';
+$req .= ' INNER JOIN marque ON casque.marque=marque.id';
+$res = $cnx->query($req);
+echo '<section id="casques">';
+while ($ligne = $res->fetch(PDO::FETCH_OBJ)) {
+echo '<article>';
+echo '<img src="../images/casques/', $ligne->libelle, '/', $ligne->image,
+'" alt="', $ligne->modele,
+'">';
+echo '</article>';
+}
+echo '</section>';
+
+
+?>
