5fff6b084e53cd4babe7af113b552e1d468c2035
IvanaelCot <ivanael.cot@gmail.com>
2026-03-08 23:01:33 +0100

    fix

--- includes/casques.inc.php
+++ includes/casques.inc.php
@@ -4,14 +4,14 @@
 $cnx = new PDO('mysql:host=127.0.0.1;port=8889;dbname=nolark', 'nolarkuser', 'nolarkpwd');
 // le champ "nom" n'est pas utilisé dans l'affichage ; on peut l'omettre
 // cela évite les problèmes de colonnes ambiguës/absentes
-$req = 'SELECT casque.id, nom, modele, libelle, prix, classement, image, stock';
+$req = 'SELECT casque.id, casque.nom, casque.modele, casque.type, casque.prix, casque.classement, casque.image, casque.stock';
 $req .= ' FROM casque INNER JOIN type ON casque.type=type.id';
 $req .= ' INNER JOIN marque ON casque.marque=marque.id';
 $res = $cnx->query($req);
 echo '<section id="casques">';
 while ($ligne = $res->fetch(PDO::FETCH_OBJ)) {
 echo '<article>';
-echo '<img src="../images/casques/', $ligne->libelle, '/', $ligne->image,'" alt="', $ligne->modele,'">';
+echo '<img src="../images/casques/', $ligne->type, '/', $ligne->image,'" alt="', $ligne->modele,'">';
 echo '</article>';
 }
 echo '</section>';
